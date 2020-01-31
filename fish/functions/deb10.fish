279c3c699fb0097df6516cdc6fac3379462cc109
Christopher McCarthy <cmccarth@mathworks.com>
2020-01-31 13:01:30 -0500

    Update host name for deb10

--- fish/functions/deb10.fish
+++ fish/functions/deb10.fish
@@ -1,4 +1,4 @@
 function deb10 --description "Connect to Debian 10 machine"
     echo -ne "\033]0;deb10\007"
-	ssh cmccarth@ah-cmccarth-l.mathworks.com $argv;
+	ssh cmccarth@ah-cmccarth-l.dhcp.mathworks.com $argv;
 end
