1275a334e9679535e1140bf2aa69c2ba787624fd
Christopher McCarthy <cmccarth@mathworks.com>
2020-02-07 11:25:16 -0500

    Fix mac alias

--- fish/functions/mac.fish
+++ fish/functions/mac.fish
@@ -1,4 +1,4 @@
-function maci --description "Connect to cmccarth-maci"
+function mac --description "Connect to cmccarth-maci"
     echo -ne "\033]0;mac\007"
 	ssh chris@ah-cmccarth-mac.dhcp.mathworks.com $argv;
 end
