def24b9de17f7b8c8bf0f93f0650edc3b5249aa5
Christopher McCarthy <cmccarth@mathworks.com>
2020-02-24 16:54:48 -0500

    Remove iTerm2 integration

--- fish/config.fish
+++ fish/config.fish
@@ -7,8 +7,6 @@ if test -d $PYENV_ROOT
     pyenv rehash
 end
 
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish ; or true
-
 if status is-interactive
     fish_vi_key_bindings
 end
