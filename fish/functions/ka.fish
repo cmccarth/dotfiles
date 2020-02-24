7891b1d122d437aef379ac1dec89d9d3016bf40b
Christopher McCarthy <cmccarth@mathworks.com>
2020-02-24 16:34:36 -0500

    Move kolla stuff

--- fish/functions/ka.fish
+++ fish/functions/ka.fish
@@ -1,3 +1,3 @@
 function ka --description "Run kolla-ansible in Docker container"
-	docker run --rm -it -v ~/git/kolla-ansible:/kolla-ansible -v ~/git/kolla-config:/etc/kolla -v /local/.ssh:/root/.ssh docker.bserepo.mathworks.com/cmccarth/kolla-ansible-exec $argv;
+	docker run --rm -it -v /local/cmccarth/kolla-ansible:/kolla-ansible -v /local/cmccarth/kolla-config:/etc/kolla -v /local/.ssh:/root/.ssh docker.bserepo.mathworks.com/cmccarth/kolla-ansible-exec $argv;
 end
