b06deb57c064a7ce9ba1345fe5043e57ebfad7c9
Christopher McCarthy <cmccarth@mathworks.com>
2020-05-09 16:41:36 -0400

    Use pwd -P instead of $PWD for Ansible functions
    $PWD doesn't resolve the symlink, ending up in an error if you're
    running from a symlink in your NFS home directory. pwd -P does resolve
    the symlink (to /local/cmccarth/...), so works.

--- fish/functions/ans.fish
+++ fish/functions/ans.fish
@@ -1,3 +1,3 @@
 function ans --description "Run ansible in Docker container"
-    docker run --rm -it -v "$PWD:/playbook" -v "$LOCALDIR/.ssh:/root/.ssh" docker.bserepo.mathworks.com/cloud/ansible-base ansible $argv;
+    docker run --rm -it -v (pwd -P)/playbook -v "$LOCALDIR/.ssh:/root/.ssh" docker.bserepo.mathworks.com/cloud/ansible-base ansible $argv;
 end
