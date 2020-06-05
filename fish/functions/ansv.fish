10e4bd8c9367930d218dfb0147a50c782f33d877
Christopher McCarthy <cmccarth@mathworks.com>
2020-06-05 13:00:24 -0400

    Ping ansible-base image to 20200521 tag

--- fish/functions/ansv.fish
+++ fish/functions/ansv.fish
@@ -1,3 +1,3 @@
 function ansv --description "Run ansible-vault in Docker container"
-    docker run --rm -it -v (pwd -P):/playbook -v "$LOCALDIR/.ssh:/root/.ssh" docker.bserepo.mathworks.com/cloud/ansible-base ansible-vault $argv;
+    docker run --rm -it -v (pwd -P):/playbook -v "$LOCALDIR/.ssh:/root/.ssh" docker.bserepo.mathworks.com/cloud/ansible-base:20200521 ansible-vault $argv;
 end
