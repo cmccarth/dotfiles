cb981010f89287b8dccbe5c96d6a290c1fcd5adc
Christopher McCarthy <cmccarth@mathworks.com>
2020-07-24 10:18:11 -0400

    Update image for ka tool

--- fish/functions/ka.fish
+++ fish/functions/ka.fish
@@ -1,3 +1,3 @@
 function ka --description "Run kolla-ansible in Docker container"
-	docker run --rm -it -v "$LOCALDIR/cmccarth/kolla-ansible:/kolla-ansible" -v "$LOCALDIR/cmccarth/kolla-config:/etc/kolla" -v "$LOCALDIR/.ssh:/root/.ssh" docker.bserepo.mathworks.com/cloud/openstack/kolla-ansible-exec:20200622 $argv;
+	docker run --rm -it -v "$LOCALDIR/cmccarth/kolla-ansible:/kolla-ansible" -v "$LOCALDIR/cmccarth/kolla-config:/etc/kolla" -v "$LOCALDIR/.ssh:/root/.ssh" docker.bserepo.mathworks.com/cloud/openstack/kolla-ansible-exec:20200624 $argv;
 end
