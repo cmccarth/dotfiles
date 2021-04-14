6a1f1a06d3e835f83158034f45c69fd98ca03f1b
Christopher McCarthy <cmccarth@mathworks.com>
2021-04-14 16:47:34 -0400

    Fix ka venv function

--- fish/functions/ka.fish
+++ fish/functions/ka.fish
@@ -1,3 +1,5 @@
 function ka --description "Run kolla-ansible installed in venv"
-	$HOME/.venv/kolla-ansible/bin/kolla-ansible --inventory $HOME/git/kolla-config/inventory --configdir $HOME/git/kolla-config --passwords $HOME/git/kolla/config/passwords.yml $argv;
+    source $HOME/.venv/kolla-ansible/bin/activate.fish
+	kolla-ansible --inventory $HOME/git/kolla-config/inventory --configdir $HOME/git/kolla-config --passwords $HOME/git/kolla-config/passwords.yml $argv
+    deactivate
 end
