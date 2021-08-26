5f6f530a43e46850acfe309feb452c925cc0fb2c
Christopher McCarthy <cmccarth@mathworks.com>
2021-08-26 14:33:33 -0400

    Different bat options for different platforms

--- fish/functions/cat.fish
+++ fish/functions/cat.fish
@@ -1,6 +1,10 @@
 function cat --description "Show contents of file with bat if available, with cat if not"
     if test (which bat)
-        bat --style plain --theme ansi $argv
+        if test (uname) = "Linux"
+            bat --style plain --theme ansi-light $argv
+        else
+            bat --style plain --theme ansi $argv
+        end
     else
         /bin/cat $argv
     end
