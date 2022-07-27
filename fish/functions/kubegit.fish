33584d459b19a1e1c109fd6779f7346a419f7ce3
Christopher McCarthy <cmccarth@mathworks.com>
2022-07-27 14:51:13 -0400

    Have kubegit default to ARC namespace

--- fish/functions/kubegit.fish
+++ fish/functions/kubegit.fish
@@ -1,3 +1,6 @@
 function kubegit --description "kubectl for GitHub Actions runners EKS cluster in AWS"
+    if not contains -- --namespace $argv
+        set -p argv --namespace=actions-runner-controller
+    end
     kubectl --kubeconfig=$HOME/.kube/github $argv
 end
