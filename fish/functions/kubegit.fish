47ebcc62809f345e8b9e491dfc89c36d63c44a63
Christopher McCarthy <cmccarth@mathworks.com>
2022-10-10 15:14:06 -0400

    Split kubegit into test and prod versions

--- fish/functions/kubegit.fish
+++
@@ -1,6 +0,0 @@
-function kubegit --description "kubectl for GitHub Actions runners EKS cluster in AWS"
-    if not contains -- --namespace $argv
-        set -p argv --namespace=actions-runner-controller
-    end
-    kubectl --kubeconfig=$HOME/.kube/github $argv
-end
