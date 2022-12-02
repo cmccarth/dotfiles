83db389d2a223168ee515e23a97a561a19a95c96
Christopher McCarthy <cmccarth@mathworks.com>
2022-12-02 13:49:43 -0500

    Update for batcat command on Debian

--- fish/functions/cat.fish
+++ fish/functions/cat.fish
@@ -1,6 +1,11 @@
 function cat --description "Show contents of file with bat if available, with cat if not"
     if test (which bat)
-        bat --style plain --theme "Solarized (light)" $argv
+        set -x BAT_COMMAND bat
+    else if test (which batcat)
+        set -x BAT_COMMAND batcat
+    end
+    if test -n $BAT_COMMAND
+        $BAT_COMMAND --style plain --theme "Solarized (light)" $argv
     else
         /bin/cat $argv
     end
