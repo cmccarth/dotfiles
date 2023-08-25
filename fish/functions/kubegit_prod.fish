82863bc7cdf6efdb3c7a658a45dd0047cc99c845
Christopher McCarthy <cmccarth@mathworks.com>
2023-08-25 15:47:49 -0400

    Update namespace for kubegit_prod

--- fish/functions/kubegit_prod.fish
+++ fish/functions/kubegit_prod.fish
@@ -1,6 +1,6 @@
 function kubegit_prod --description "kubectl for GitHub Actions runners PROD EKS cluster in AWS"
     if not contains -- --namespace $argv
-        set -p argv --namespace=actions-runner-controller
+        set -p argv --namespace=arc-prod
     end
     kubectl --kubeconfig=$HOME/.kube/github_prod $argv
 end
